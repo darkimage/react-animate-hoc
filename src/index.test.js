e5ad22555d7663886b2844d3e5095e9609be927f
Darkimage <luc-af@live.it>
2020-09-13 00:16:11 +0200

    Added test

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,14 @@
-import { ExampleComponent } from '.'
+import { withAnimated } from '.';
+import React from 'react';
+import { cleanup, fireEvent, render } from "@testing-library/react";
 
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+test('Should Render simple component', () => {
+  const Component = withAnimated(function (){
+    return <div>Simple Component</div>
   })
+  const {getByText} = render(
+    <Component/>,
+  );
+
+  expect(getByText(/Simple Component/)).toBeTruthy()
 })
