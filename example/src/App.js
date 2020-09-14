60cbbaa1e85a8082492990d6b11c2607e8c6364a
Darkimage <luc-af@live.it>
2020-09-14 03:04:44 +0200

    Work on docs examples

--- example/src/App.js
+++ example/src/App.js
@@ -10,12 +10,13 @@ import './prism.css'
 const BounceIn = withAnimated(
   function (props) {
     return (
-      <div style={props.style} className={props.className}> 
+      <div {...props}> 
         I'm a Bouncing div
       </div>)
   },
   {
-    animation: 'bounceIn'
+    animation: 'bounceIn',
+    infinite: true
   }
 )
 
@@ -24,17 +25,38 @@ const Code = function (props) {
   return !props.noPre ? <pre className="NormalizeWhitespace">{code}</pre> : code
 }
 
-const AppContainer = styled.div`
+const AppContainer = withAnimated(styled.div`
   margin: 5% 25%;
-`;
+  @media only screen and (max-width: 768px){
+    margin: 5% 15%;
+  }
+  @media only screen and (max-width: 425px){
+    margin: 5% 8%;
+  }
+`, {
+    animation: "fadeIn",
+    animationDuration: "2s"
+});
 
 const codeWithAnimated = 'withAnimated()';
 const codeWithAnimatedGroup = 'withAnimatedGroup()';
 const testCode = `
-const AppContainer = styled.div\`
-	margin: 5% 25%;
-\`;
-`
+// Simple component that render a div 
+// (style and className are necessary for the library to work)
+const div = function (props) {
+  return (
+    <div {...props}> 
+      I'm a Bouncing div
+    </div>
+  )
+};
+
+//Lets add some animation to it 🥰
+const BouncingDiv = withAnimated(div,{
+    animation: 'bounceIn',
+    infinite: true
+  }
+);`
 
 const App = () => {
   return (
@@ -44,13 +66,14 @@ const App = () => {
       </Head>
       <h1>React Animate <a href="https://reactjs.org/docs/higher-order-components.html">HOC</a></h1>
       <p>
-				This React library is designed to leverage the awesome <a href="https://animate.style/">animate.css</a> with little work, by simple wrapping your components using either <Code noPre code={codeWithAnimated} /> or <Code noPre code={codeWithAnimatedGroup}/>
+				This React library is designed to leverage the awesome <a href="https://animate.style/">animate.css</a> with little work, simply by wrapping your components using either <Code noPre code={codeWithAnimated} /> or <Code noPre code={codeWithAnimatedGroup}/>
 			</p>
-			<p className="examples">
-				Prova Prova
+			<div className="examples">
+				<h2>Basic Example</h2>
 				<Code code={testCode}/>
-			</p>
-      <BounceIn className="animateContainer"/>
+        <strong>Result:</strong><BounceIn className="animateContainer" />
+        <h2>Styled Components, why not 🤩</h2>
+			</div>
     </AppContainer>
   )
 } 
