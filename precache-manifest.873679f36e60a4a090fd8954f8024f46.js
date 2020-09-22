ba1ea39e29eda97299b2e9972df2de04476e1d4e
DarkImage <luc-af@live.it>
2020-09-22 14:25:00 +0200

    Updates

---
+++ precache-manifest.873679f36e60a4a090fd8954f8024f46.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0ac96db749f484f9b6e5382b7dce43e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8390bb67d6b039db576d",
+    "url": "./static/css/2.61b7ab8d.chunk.css"
+  },
+  {
+    "revision": "33168788c04470a90073",
+    "url": "./static/css/main.53e66046.chunk.css"
+  },
+  {
+    "revision": "8390bb67d6b039db576d",
+    "url": "./static/js/2.0488a76b.chunk.js"
+  },
+  {
+    "revision": "3caaeab42b48d0c867550bcf1f898c6a",
+    "url": "./static/js/2.0488a76b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33168788c04470a90073",
+    "url": "./static/js/main.9fef5dc0.chunk.js"
+  },
+  {
+    "revision": "a131a394092bbda619ef",
+    "url": "./static/js/runtime-main.71677cc3.js"
+  },
+  {
+    "revision": "7ad5edd950ea96ad8719c8fe5d242a78",
+    "url": "./static/media/github_mark.7ad5edd9.svg"
+  },
+  {
+    "revision": "eeb29dca6b195dc545ade0eb03a600b4",
+    "url": "./static/media/see_it_on_github.eeb29dca.svg"
+  },
+  {
+    "revision": "cd13bf11a9984b17b5caf39e32412aff",
+    "url": "./static/media/social_bg.cd13bf11.svg"
+  }
+]);
